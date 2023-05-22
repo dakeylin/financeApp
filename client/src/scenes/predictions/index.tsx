e85a9326139a4c902fe500b86ac9c0bc48169719
dakeylin <dakeylin@gmail.com>
2023-05-23 01:42:17 +0300

    Создана маршрутизация и панель для будущих диаграмм

---
+++ client/src/scenes/predictions/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+
+type Props = {}
+
+const Predictions = (props: Props) => {
+  return (
+    <Box>Predictions</Box>
+  )
+}
+
+export default Predictions
